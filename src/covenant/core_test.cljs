f945898ea66d158888636985d6614ccd8446e4dd
David Meister <thedavidmeister@gmail.com>
2018-04-01 16:42:48 +1000

    wip on tests

--- src/covenant/core_test.cljs
+++ src/covenant/core_test.cljs
@@ -3,17 +3,65 @@
   covenant.core
   [cljs.test :refer-macros [deftest is are]]))
 
-(deftest ??fn?
- ; valids
- (doseq [v [#()
-            (fn [] :foo)]]
-  (is (covenant.core/validate :covenant.core/fn v)))
+(def bools [true false])
+(def numbers [-1 0 1 2 2.5 0.0 ##NaN js/Infinity])
+(def nothings [js/undefined nil])
+(def strings ["foo" ""])
+(def chrs [\a \b \c])
+(def objects [(js-obj (clj->js {:foo :bar}))])
+(def maps [{:foo :bar} {1 2} {:foo :foo} {}])
+(def sets [#{:foo} #{} #{1}])
+(def vectors [[] [:foo :bar] [1 2]])
+(def lists [(list) (list :foo :bar) (list 1 2)])
+(def fns [#() (fn [] :foo)])
+(def everything
+ (flatten
+  [bools
+   numbers
+   nothings
+   strings
+   chrs
+   objects
+   maps
+   sets
+   vectors
+   lists
+   fns]))
 
- ; invalids
- (doseq [v [true false
-            -1 0 1 2 2.5 0.0 ##NaN js/Infinity
-            js/undefined nil
-            "foo" ""
-            (js-obj) (clj->js {:foo :bar})
-            {:foo :bar} #{:foo} [:foo :bar] (list :foo :bar)]]
-  (is (not (covenant.core/validate :covenant.core/fn v)))))
+(prn everything)
+
+(defn check-validate
+ ([covenant valids]
+  (check-validate covenant valids
+   ; everything that is not valid is invalid
+   (remove
+    #(contains? (set (flatten valids)) %)
+    (flatten everything))))
+ ([covenant valids invalids]
+  (doseq [v (flatten valids)]
+   (is
+    (covenant.core/validate covenant v)
+    (str "Failed to validate " (pr-str v) " against covenant " covenant)))
+
+  (doseq [v (flatten invalids)]
+   (is
+    (not (covenant.core/validate covenant v))
+    (str "Failed to invalidate " (pr-str v) " against covenant " covenant)))))
+
+(deftest ??fn
+ (check-validate :covenant.core/fn fns))
+
+(deftest ??any
+ (check-validate :covenant.core/any everything))
+
+(deftest ??map
+ (check-validate :covenant.core/map maps))
+
+(deftest ??nil
+ (check-validate :covenant.core/nil nil))
+
+(deftest ??set
+ (check-validate :covenant.core/set sets))
+
+(deftest ??char
+ (check-validate :covenant.core/char chrs))
