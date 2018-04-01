67a9f2e6cda55f3591a8188d0d4f2eccf946fcf2
David Meister <thedavidmeister@gmail.com>
2018-04-01 16:54:16 +1000

    WIP on basic tests

--- src/covenant/core_test.cljs
+++ src/covenant/core_test.cljs
@@ -1,49 +1,48 @@
 (ns covenant.core-test
  (:require
   covenant.core
+  clojure.set
   [cljs.test :refer-macros [deftest is are]]))
 
-(def bools [true false])
-(def numbers [-1 0 1 2 2.5 0.0 ##NaN js/Infinity])
-(def nothings [js/undefined nil])
-(def strings ["foo" ""])
-(def chrs [\a \b \c])
-(def objects [(js-obj (clj->js {:foo :bar}))])
-(def maps [{:foo :bar} {1 2} {:foo :foo} {}])
-(def sets [#{:foo} #{} #{1}])
-(def vectors [[] [:foo :bar] [1 2]])
-(def lists [(list) (list :foo :bar) (list 1 2)])
-(def fns [#() (fn [] :foo)])
+(def bools #{true false})
+(def numbers #{-1 0 1 2 2.5 0.0 ##NaN js/Infinity})
+(def falseys #{nil false})
+(def strings #{"foo" ""})
+(def chrs #{\a \b \c})
+(def objects #{(js-obj) (clj->js {:foo :bar})})
+(def maps #{{:foo :bar} {1 2} {:foo :foo} {}})
+(def sets #{#{:foo} #{} #{1}})
+(def vectors #{[] [:foo :bar] [1 2]})
+(def lists #{(list) (list :foo :bar) (list 1 2)})
+(def fns #{#() (fn [] :foo)})
 (def everything
- (flatten
-  [bools
-   numbers
-   nothings
-   strings
-   chrs
-   objects
-   maps
-   sets
-   vectors
-   lists
-   fns]))
-
-(prn everything)
+ (clojure.set/union
+  bools
+  numbers
+  falseys
+  strings
+  chrs
+  objects
+  maps
+  sets
+  vectors
+  lists
+  fns))
 
 (defn check-validate
  ([covenant valids]
   (check-validate covenant valids
    ; everything that is not valid is invalid
-   (remove
-    #(contains? (set (flatten valids)) %)
-    (flatten everything))))
+   (clojure.set/difference
+    everything
+    valids)))
  ([covenant valids invalids]
-  (doseq [v (flatten valids)]
+  (doseq [v (vals valids)]
    (is
     (covenant.core/validate covenant v)
     (str "Failed to validate " (pr-str v) " against covenant " covenant)))
 
-  (doseq [v (flatten invalids)]
+  (doseq [v (vals invalids)]
    (is
     (not (covenant.core/validate covenant v))
     (str "Failed to invalidate " (pr-str v) " against covenant " covenant)))))
@@ -52,7 +51,10 @@
  (check-validate :covenant.core/fn fns))
 
 (deftest ??any
- (check-validate :covenant.core/any everything))
+ (let [invalids falseys]
+  (check-validate :covenant.core/any
+   (clojure.set/difference everything invalids)
+   invalids)))
 
 (deftest ??map
  (check-validate :covenant.core/map maps))
